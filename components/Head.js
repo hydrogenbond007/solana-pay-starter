766d99650e9da37e1a7ab8c8bae032b7e059f906
Madhav Goyal <madhavgoyal@Madhavs-MacBook-Air.local>
2022-05-25 01:25:33 +0530

    Solana-Pay-store-build

--- components/Head.js
+++ components/Head.js
@@ -5,7 +5,7 @@ export default function HeadComponent() {
   return (
     <Head>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="theme-color" content="#000000" />
+      <meta name="theme-color" content="#000111" />
 
       <title>Solana Pay Store</title>
       <meta name="title" content="Solana Pay Store" />
